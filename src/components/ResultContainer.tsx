45eefd3eb7b19c39b680a313153013ffecf7f219
Leonardo Habitzreuter <leo.habitzreuter@gmail.com>
2024-07-21 19:02:23 -0300

    subscription container

--- src/components/ResultContainer.tsx
+++ src/components/ResultContainer.tsx
@@ -12,13 +12,16 @@ export default function ResultContainer ({ fields }: Props) {
   const cycles = Number(fields.billingCycles || 0)
 
   return (
-    <p className="sm:col-span-3">
-      Your customer will be charged ${initialPrice} immediately&nbsp;
-      {hasTrial && `for their ${fields.trialPeriodNumber || 0} ${fields.trialPeriod} trial, `}
-      and then ${paymentAmount} every {fields.billingFrequencyNumber || 0} {fields.billingFrequencyPeriod || 'TBD'}
-      {neverEnds ? ' until they cancel.' : (
-        `, ${cycles} times. The total amount paid will be $${initialPrice + (paymentAmount * cycles)}.`
-      )}
-    </p>
+    <div className="sm:col-span-3 bg-[#EBEBEB] p-4 rounded flex gap-2 items-start">
+      <img width={15} height={21} src='/subscription.svg' />
+      <p>
+        Your customer will be charged ${initialPrice} immediately&nbsp;
+        {hasTrial && `for their ${fields.trialPeriodNumber || 0} ${fields.trialPeriod} trial, `}
+        and then ${paymentAmount} every {fields.billingFrequencyNumber || 0} {fields.billingFrequencyPeriod || 'TBD'}
+        {neverEnds ? ' until they cancel.' : (
+          `, ${cycles} times. The total amount paid will be $${initialPrice + (paymentAmount * cycles)}.`
+        )}
+      </p>
+    </div>
   )
 }
